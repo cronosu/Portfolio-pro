455d1ceb6e8e8e5006a111033b766734d0a82fed
stoclet sylvain <cronosu@hotmail.fr>
2023-12-29 09:58:42 +0100

    lien google map +mail

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,15 +3,16 @@ import React from "react"
 function Footer() {
 
   return (
-
     <footer className="footer">
       <ul className="footer-links">
-        <li>Localisation : Cambrai,Hauts-de-France</li>
+        <li>Localisation : 
+          <a href="https://www.google.com/maps?q=Cambrai,Hauts-de-France"> Cambrai, Hauts-de-France</a>
+        </li>
         <li>Tel : 07.69.43.35.66</li>
-        <li>Email : <a href="mailto:sylvain.stoclet@yahoo.com">sylvain.stoclet@yahoo.com</a></li>
-
+        <li>Email : 
+          <a href="mailto:sylvain.stoclet@yahoo.com"> sylvain.stoclet@yahoo.com</a>
+        </li>
       </ul>
-
     </footer>
   )
 }

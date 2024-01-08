7c3a3690e3cd88cb9dd2c7d543eb18537c94ca2d
stoclet sylvain <cronosu@hotmail.fr>
2024-01-08 17:58:16 +0100

    update

--- src/components/APropos.jsx
+++ src/components/APropos.jsx
@@ -6,7 +6,7 @@ function Apropos() {
       <p className="APropos">
         Bienvenue !{<br />}{<br />}
         Développeur Web Front end
-        , passionné , fan de pixels, de lignes de code depuis tout petit <br /> Vous êtes prêt à plnger dans mon univers où le sérieux
+        , passionné , fan de pixels, de lignes de code depuis tout petit <br /> Vous êtes prêt à plonger dans mon univers où le sérieux
         et le fun s'entremêle ?
       </p>
     </div >

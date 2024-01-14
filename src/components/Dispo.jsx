db9d041f9bb3f62701f5a08c5c5d1e9f80ff8e37
stoclet sylvain <cronosu@hotmail.fr>
2024-01-14 21:32:50 +0100

    orthographe

--- src/components/Dispo.jsx
+++ src/components/Dispo.jsx
@@ -4,7 +4,7 @@ function Dispo() {
     return (
   <div className="dispo">
 <img alt="case-a-cocher " src="/cocher.png"/>
-<p>Prêts a rejoindre votre projet !</p>
+<p>Prêts à rejoindre votre projet !</p>
   </div>
     )
   }
--- src/components/MonParcour.jsx
+++ src/components/MonParcour.jsx
@@ -14,7 +14,7 @@ function MonParcour() {
 
 J'ai obtenu mon diplôme du Baccalauréat professionnel de commerce en 2007.<br/><br/>
 
-Au cours de mes études, j'ai eu l'opportunité de m'impliquer dans activité commerciale local, ce qui m'a permis de développer des compétences en relationnel client.<br/><br/>
+Au cours de mes études, j'ai eu l'opportunité de m'impliquer dans l'activité commerciale local, ce qui m'a permis de développer des compétences en relationnel client.<br/><br/>
 
 Après mes études, j'ai eu l'occasion de travailler chez la grande entreprise McDonald's <img alt="icon-mcdonald's" className="Mcdo-icone" src="./mcdonalds.png"></img> en tant que "employé polyvalent" puis "chef d'équipe" .<br/><br/>
 
@@ -22,7 +22,7 @@ Cette expérience m'a permis d'acquérir une expertise en gestion du personnel e
 
 Actuellement, suite à une reconversion professionnelle, je suis très motivé à poursuivre mes ambitions premières dans le développement web, car je suis convaincu que cette passion m'épanouira pleinement dans ma vie professionnel comme personnel.<br/><br/>
 
-Sur mon temps libre, j'aime les consacrer à ma passion cinéphile, développer des projets personnels, mes enfants et jouer aux jeux vidéo et j'aspire à être heureux dans tous les domaines .<br/><br/>
+Mon temps libre, j'aime le consacrer à ma passion cinéphile, développer des projets personnels, mes enfants et jouer aux jeux vidéo et j'aspire à être heureux dans tous les domaines .<br/><br/>
 
 Je suis enthousiaste à l'idée d'avoir partagé mon parcours et d'échanger avec vous dans le futur. <br/><br/>
 
